f3effa820c0816dfe91b8fb8ad3f3c1031166a6a
pius wanyangu <ndubipius96@gmail.com>
2025-12-01 23:44:46 +0300

    adding task_1 teachers interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,14 +1,28 @@
 export interface Teacher {
-    firstName: string
-    lastName: string
-    fullTimeEmployee: boolean
-    yearsOfExperience:number
-    location:string
-    contract?: boolean
+    // readonly mode
+    readonly firstName: string;
+    readonly lastName: string;
+    // mandatory
+    fullTimeEmployee: boolean;
+    location:string;
+// optional property we use ? modifier
+    yearsOfExperience?:number;
+    // allow any other string-keyed property
+    [key: string]: any;
 
 }
 
 // teacher
+const teacher1: Teacher = {
+    firstName: "John",
+    lastName: "Doe",
+    fullTimeEmployee: true,
+    location: "New York",
+    yearsOfExperience: 5, // Optional, so we can include it
+    subject: "Math", // Allowed by the [key: string]: any; signature
+    age: 40 // Also allowed
+};
+
 const teacher3: Teacher = {
     firstName: 'John',
     fullTimeEmployee: false,
