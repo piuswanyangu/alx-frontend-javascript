de11cd8b11bd8a300c8d6ba3c4ff5d00b0c73ba8
pius wanyangu <ndubipius96@gmail.com>
2025-12-02 11:26:41 +0300

    adding advanced types part 1

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,56 @@
+interface DirectorInterface{
+    // methods 
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+// teachers interface
+interface TeacherInterface{
+    workFromHome(): string;
+    getCoffeeBreak(): string
+    workTeachersTasks(): string;
+}
+
+class Director implements DirectorInterface{
+    getCoffeeBreak(): string {
+        throw new Error("Method not implemented.");
+    }
+    
+    workFromHome(): string{
+        return `working from home`
+    }
+    getToWork(): string{
+        return `Getting a coffee break`
+    }
+    workDirectorTasks(): string{
+        return ` Getting to director task`
+    }
+}
+
+// teachers class
+class Teacher implements TeacherInterface{
+    workTeachersTasks(): string {
+        throw new Error("Method not implemented.");
+    }
+    
+    workFromHome(): string{
+        return `Cannot work from home`
+    }
+    getCoffeeBreak(): string{
+        return ` Cannot have a break`
+    }
+    workTeacherTasks(): string{
+        return `Getting to work`
+    }
+}
+
+// create a function employee 
+function createEmployee(salary: number | string): Director | Teacher {
+    if(typeof salary === 'number' && salary< 500){
+        return new Teacher();
+    }
+    return new Director();
+}
+
+createEmployee(200)
